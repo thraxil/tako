63335c25e703ab59a15e29f2b2b582e58cdc631e
Anders Pearson <anders@columbia.edu>
2011-11-25 20:43:40 -0500

    dummy index view

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from tako.main.models import Node
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+from annoying.decorators import render_to
 
+@render_to("main/index.html")
 def index(request):
-    
+    return dict()
