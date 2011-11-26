305a8a70bf49f298199dc39fa2a58bd8a4e42fbe
Anders Pearson <anders@columbia.edu>
2011-11-25 21:03:38 -0500

    no twill

--- main/features/index.py
+++ main/features/index.py
@@ -1,5 +1,7 @@
 from lettuce import *
 from django.test.client import Client
+from twill import get_browser
+from lettuce.django import django_url
 
 @before.all
 def set_browser():
@@ -7,12 +9,12 @@ def set_browser():
 
 @step(r'I access the url "(.*)"')
 def access_url(step, url):
-    response = world.browser.get(url)
-    world.response = response
+    world.response = world.browser.get(django_url(url))
 #    world.dom = html.fromstring(response.content)
 
 @step(r'I see the header "(.*)"')
 def see_header(step, text):
  #   header = world.dom.cssselect('h1')[0]
 #    assert header.text == text
+#    print str(dir(world.response))
     assert text in world.response.content
