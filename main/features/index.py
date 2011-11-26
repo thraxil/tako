5f1ab5fe3272fe849c278488d1f1e01a54ae13c7
Anders Pearson <anders@columbia.edu>
2011-11-25 20:51:15 -0500

    store full response

--- main/features/index.py
+++ main/features/index.py
@@ -8,11 +8,11 @@ def set_browser():
 @step(r'I access the url "(.*)"')
 def access_url(step, url):
     response = world.browser.get(url)
-    world.content = response.content
+    world.response = response
 #    world.dom = html.fromstring(response.content)
 
 @step(r'I see the header "(.*)"')
 def see_header(step, text):
  #   header = world.dom.cssselect('h1')[0]
 #    assert header.text == text
-    assert text in world.content
+    assert text in world.response.content
