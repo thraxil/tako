a33368fc8ae0dba03f75eb50f425aa03546b77d0
Anders Pearson <anders@columbia.edu>
2011-11-25 20:29:17 -0500

    basic model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from treebeard.mp_tree import MP_Node
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Node(MP_Node):
+    label = models.CharField(max_length=256,default="")
+    slug = models.SlugField(max_length=256,editable=False)
+    user = models.ForeignKey(User)
+    details = models.TextField(blank=True,default="")
+    added = models.DateTimeField(auto_now_add=True,editable=False)
+    modified = models.DateTimeField(auto_now=True,editable=False)
+
+    def save(self,*args,**kwargs):
+        slug = slugify(self.label)[:255]
+        # TODO: uniquify for the particular level of the tree
+#        addto = 1
+#        while Node.objects.filter(slug=slug).count() > 0:
+#            slug = slug + str(addto)
+#            addto += 1
+        self.slug = slug
+        super(Node, self).save(*args,**kwargs)
