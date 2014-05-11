2912b4a90c755168485763c11cb447c75b757dd2
Anders Pearson <anders@columbia.edu>
2014-05-10 20:16:38 -0400

    duh

--- fabfile.py
+++ fabfile.py
@@ -9,15 +9,15 @@ def restart_gunicorn():
     sudo("restart tako")
 
 def prepare_deploy():
-    local("manage test")
-    local("manage flake8")
+    local("make test")
+    local("make flake8")
 
 def deploy():
     code_dir = "/var/www/tako/tako"
     with cd(code_dir):
         run("git pull origin master")
-        run("manage migrate")
-        run("manage collectstatic")
+        run("make migrate")
+        run("make collectstatic")
         for n in nginx_hosts:
             run(("rsync -avp --delete media/ "
                  "%s:/var/www/tako/tako/media/") % n)
